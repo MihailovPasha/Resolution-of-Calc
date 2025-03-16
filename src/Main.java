38654e63cb47e99c436ce04a41c505b3b2b10919
unknown <mihailov.pashka@yandex.ru>
2025-03-16 20:29:25 +0300

    init

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Введите задание (например, 1 + 2 или V - I):");
+        String input = s.nextLine();
+
+        try {
+            String result = Calc.calс(input);
+            System.out.println("Результат: " + result);
+        } catch (Exception e) {
+            System.out.println("Ошибка: " + e.getMessage());
+        } finally {
+            s.close();
+        }
+    }
+}
---
+++ src/Roma.java
@@ -0,0 +1,51 @@
+/**
+ * Класс отвечает за перевод арабских чисел в римские и наоборот;
+ */
+public class Roma {
+    private static int[] values = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+    private static String[] symbols = {"X", "IX","VIII", "VII", "VI", "V", "IV","III", "II", "I"};
+
+    /**
+     * Метод переводит введённое римское значение к соответсвующему арабскому значению;
+     *
+     * @param symbol String, римское число;
+     * @return Возвращает целочисленное значение арабской системы чисел;
+     */
+    public static int getValue(String symbol) {
+        return switch (symbol) {
+            case "X" -> 10;
+            case "IX" -> 9;
+            case "VIII" -> 8;
+            case "VII" -> 7;
+            case "VI" -> 6;
+            case "V" -> 5;
+            case "IV" -> 4;
+            case "III" -> 3;
+            case "II" -> 2;
+            case "I" -> 1;
+            default -> throw new IllegalArgumentException("Неверный римский символ: " + symbol);
+        };
+    }
+
+    /**
+     * Метод переводит арабские числа в римские
+     * @param num int, принимает в качестве входящих параметров целочисленные значения;
+     * @return Возвращает строковое значение римской системы чисел;
+     */
+    public static String toRoman(int num) {
+        if (num <= 0) {
+            throw new IllegalArgumentException("Римские числа не могут быть меньше единицы.");
+        }
+        StringBuilder result = new StringBuilder();
+        int i = 0;
+        while (num > 0) {
+            if (values[i] <= num) {
+                result.append(symbols[i]);
+                num = num - values[i];
+            } else {
+                i++;
+            }
+        }
+        return result.toString();
+    }
+}
